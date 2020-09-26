f91e85d8792c7ac3f3e4677a4c1d1110bee5068e
HoangNam1101 <namhhpk01637@fpt.edu.vn>
2020-09-26 17:53:05 +0700

    Interface

---
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.food;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnMonAn = findViewById(R.id.btnMonAn);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
---
+++ app/src/main/java/com/example/food/object/MonAn.java
@@ -0,0 +1,10 @@
+package com.example.food.object;
+
+public class MonAn {
+    public int iDMonAn;
+    public String TenMonAn;
+    public String TenQuan;
+    public String DiaChi;
+    public int HinhAnh;
+
+}
