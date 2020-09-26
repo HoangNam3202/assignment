21cab7b165bc6cd3f5adfdbf9ac29ca49913924b
HoangNam1101 <namhhpk01637@fpt.edu.vn>
2020-09-26 17:56:30 +0700

    Them object

--- app/src/main/java/com/example/food/object/MonAn.java
+++ app/src/main/java/com/example/food/object/MonAn.java
@@ -7,4 +7,51 @@ public class MonAn {
     public String DiaChi;
     public int HinhAnh;
 
+    public MonAn(int iDMonAn, String tenMonAn, String tenQuan, String diaChi, int hinhAnh) {
+        this.iDMonAn = iDMonAn;
+        TenMonAn = tenMonAn;
+        TenQuan = tenQuan;
+        DiaChi = diaChi;
+        HinhAnh = hinhAnh;
+    }
+
+    public int getiDMonAn() {
+        return iDMonAn;
+    }
+
+    public void setiDMonAn(int iDMonAn) {
+        this.iDMonAn = iDMonAn;
+    }
+
+    public String getTenMonAn() {
+        return TenMonAn;
+    }
+
+    public void setTenMonAn(String tenMonAn) {
+        TenMonAn = tenMonAn;
+    }
+
+    public String getTenQuan() {
+        return TenQuan;
+    }
+
+    public void setTenQuan(String tenQuan) {
+        TenQuan = tenQuan;
+    }
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        DiaChi = diaChi;
+    }
+
+    public int getHinhAnh() {
+        return HinhAnh;
+    }
+
+    public void setHinhAnh(int hinhAnh) {
+        HinhAnh = hinhAnh;
+    }
 }
