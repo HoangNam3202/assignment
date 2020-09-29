7e751044882d2e59659535b50cc51bc64af88ece
HoangNam1101 <namhhpk01637@fpt.edu.vn>
2020-09-29 16:30:05 +0700

    sua layout

--- app/src/main/java/com/example/food/MonAnActivity.java
+++ app/src/main/java/com/example/food/MonAnActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.food.adapter.MonAnAdapter;
 import com.example.food.object.MonAn;
@@ -18,7 +21,7 @@ public class MonAnActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mon_an);
         ListView listView_MonAn = findViewById(R.id.listMonAn);
-        ArrayList<MonAn> arr = new ArrayList<>();
+        final ArrayList<MonAn> arr = new ArrayList<>();
         MonAnAdapter monAnAdapter = new MonAnAdapter(MonAnActivity.this,R.layout.row_of_mon_an,arr);
         listView_MonAn.setAdapter(monAnAdapter);
 
@@ -27,7 +30,7 @@ public class MonAnActivity extends AppCompatActivity {
                 "TenMonAn varchar(35), TenQuan varchar(20), DiaChi varchar(50), Hinh Integer, Gia Integer)");
         dataBaseHelper.UpData("Delete from MonAn where TenMonAn = 'Com Hai San'");
 
-        dataBaseHelper.UpData("Insert into MonAn Values(null,'Com Hai San','Muoi Kho','82 Hung Vuong','"+R.drawable.comga+"',35.000)");
+        dataBaseHelper.UpData("Insert into MonAn Values(null,'Com Hai San','Muoi Kho','82 Hung Vuong','"+R.drawable.comga+"',351)");
         Cursor cursor = dataBaseHelper.GetData("Select * from MonAn");
         while (cursor.moveToNext()){
             int id = cursor.getInt(0);
@@ -39,5 +42,11 @@ public class MonAnActivity extends AppCompatActivity {
 
             arr.add(new MonAn(id,TenMonAn,TenQuan,DiaChi,Hinh,Gia));
         }
+        listView_MonAn.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MonAnActivity.this, ""+arr.get(i).TenMonAn, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
